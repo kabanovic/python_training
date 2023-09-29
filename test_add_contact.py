cfcad6cde8c565bf6fb92710e18ac7e8e263a67b
kabanovic <akim_l.a@mail.ru>
2023-09-30 00:52:37 +0300

    переход к фикстуре у контактов

--- test_add_contact.py
+++ test_add_contact.py
@@ -6,66 +6,18 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
+import pytest
 from contact import Contact
+from fixt import Applicat
 
-class TestAddContact(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+@pytest.fixture
+def appl(request):
+    fixture = Applicat()
+    request.addfinalizer(fixture.destr)
+    return fixture
 
-    
-    def test_add_contact(self):
-        wd = self.wd
-        self.home_page(wd)
-        self.login(wd, "admin", "secret")
-        self.add_contact(wd, Contact("Name2", "Middle2", "Surname2", "89991234567"))
-        self.return_home_page(wd)
-        self.logout(wd)
+def test_add_contact(appl):
+    appl.login("admin", "secret")
+    appl.add_contact(Contact("Name2", "Middle2", "Surname2", "89991234567"))
+    appl.logout()
 
-    def logout(self, wd):
-        wd.find_element(By.LINK_TEXT, "Logout").click()
-
-    def return_home_page(self, wd):
-        wd.find_element(By.LINK_TEXT, "home page").click()
-
-    def add_contact(self, wd, contact):
-        wd.find_element(By.LINK_TEXT, "add new").click()
-        wd.find_element(By.NAME, "firstname").click()
-        wd.find_element(By.NAME, "firstname").clear()
-        wd.find_element(By.NAME, "firstname").send_keys(contact.firstname)
-        wd.find_element(By.NAME, "middlename").clear()
-        wd.find_element(By.NAME, "middlename").send_keys(contact.middlename)
-        wd.find_element(By.NAME, "lastname").clear()
-        wd.find_element(By.NAME, "lastname").send_keys(contact.lastname)
-        wd.find_element(By.NAME, "mobile").click()
-        wd.find_element(By.NAME, "mobile").clear()
-        wd.find_element(By.NAME, "mobile").send_keys(contact.telephone)
-        wd.find_element(By.XPATH, "//div[@id='content']/form/input[21]").click()
-
-    def login(self, wd, username, password):
-        wd.find_element(By.NAME, "user").clear()
-        wd.find_element(By.NAME, "user").send_keys(username)
-        wd.find_element(By.NAME, "pass").clear()
-        wd.find_element(By.NAME, "pass").send_keys(password)
-        wd.find_element(By.ID, "LoginForm").submit()
-
-    def home_page(self, wd):
-        wd.get("http://localhost/addressbook/index.php")
-
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-
-    
-    def tearDown(self):
-        self.wd.quit()
-
-
-if __name__ == "__main__":
-    unittest.main()
