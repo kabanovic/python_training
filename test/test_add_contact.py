f7f664dca3232012a030927d35d2770b18c41efe
kabanovic <akim_l.a@mail.ru>
2023-10-21 19:13:11 +0300

    удаление сравнения телефонов и исправление ошибки (2)

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -5,7 +5,7 @@ from model.contact import Contact
 
 def test_add_contact(app):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact("Name3", "Middle3", "Surname3", "89991234567")
+    contact = Contact("Name3", "Middle3", "Surname3")
     app.contact.add(contact)
     assert len(old_contacts) + 1 == app.contact.count_cont()
     new_contacts = app.contact.get_contact_list()
