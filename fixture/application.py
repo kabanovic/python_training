34456d6c0e611d7634a078f80e119037e760b94d
kabanovic <akim_l.a@mail.ru>
2023-10-23 23:19:24 +0300

    запуск браузера из командной строки (дз)

--- fixture/application.py
+++ fixture/application.py
@@ -6,12 +6,20 @@ from selenium.webdriver.common.by import By
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.wd.implicitly_wait(1)
         self.session = Session(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -24,7 +32,7 @@ class Application:
         wd = self.wd
         if wd.current_url.endswith("/index.php"):
             return
-        wd.get("http://localhost/addressbook/index.php")
+        wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
