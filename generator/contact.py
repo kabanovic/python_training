d2fe5df34692cb321b6f35f2e7860f705fda07a1
kabanovic <akim_l.a@mail.ru>
2023-10-25 11:49:05 +0300

    сохраняем все на всякий случай

--- generator/contact.py
+++ generator/contact.py
@@ -1,10 +1,11 @@
-import jsonpickle
+#import jsonpickle
 from model.contact import Contact
 import random
 import string
 import os.path
 import getopt
 import sys
+import json
 
 
 try:
@@ -36,6 +37,6 @@ testdata = [Contact(firstname="", lastname="")] + [
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as out:
-    #out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
-    jsonpickle.set_encoder_options("json", indent=2)
-    out.write(jsonpickle.encode(testdata))
+    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+    #jsonpickle.set_encoder_options("json", indent=2)
+    #out.write(jsonpickle.encode(testdata))
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,8 +3,8 @@
 from model.contact import Contact
 
 
-def test_add_contact(app, json_contact):
-    contact = json_contact
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.add(contact)
     assert len(old_contacts) + 1 == app.contact.count_cont()
