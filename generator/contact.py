dc6938f036f2f56a2705e2d921ed20e7ab4d31b4
kabanovic <akim_l.a@mail.ru>
2023-10-25 12:10:07 +0300

    рабочий генератор и запуск тестов (дз)

--- generator/contact.py
+++ generator/contact.py
@@ -5,7 +5,7 @@ import string
 import os.path
 import getopt
 import sys
-import json
+import jsonpickle
 
 
 try:
@@ -37,6 +37,6 @@ testdata = [Contact(firstname="", lastname="")] + [
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as out:
-    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
-    #jsonpickle.set_encoder_options("json", indent=2)
-    #out.write(jsonpickle.encode(testdata))
+    #out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
