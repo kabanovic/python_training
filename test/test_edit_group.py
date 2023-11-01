11b77b50177e7fb27b6ef888179d8a54a83427f0
kabanovic <akim_l.a@mail.ru>
2023-11-02 02:12:07 +0300

    тесты с бд для групп и начало контактов

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,19 +2,21 @@ from model.group import Group
 import random
 from random import randrange
 
-def test_edit_some_group(app, db):
+def test_edit_some_group(app, db, check_ui):
     if app.group.count_group() == 0:
         app.group.create(Group("rty", "fgh", "vbn"))
     old_groups = db.get_group_list()
-    group = random.choice(old_groups)
-    #index = randrange(len(old_groups))
-    group = Group("545", "888", "777")
-    group.id = old_groups[index].id
-    app.group.edit_some_group(index, group)
+    index = randrange(len(old_groups))
+    gp = old_groups[index]
+    group = Group("66", "66", "66")
+    group.id = gp.id
+    app.group.edit_some_group_by_id(gp.id, group)
     assert len(old_groups) == app.group.count_group()
     new_groups = db.get_group_list()
     old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
 
 
 #def test_edit_first_group_header(app):
